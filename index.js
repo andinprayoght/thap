23735ed39aeee6f9033fa5faa05918c7fb973a27
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2024-10-17 02:49:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const server = http.createServer(async (req, res) => {
 });
 
 // Mendengarkan pada port yang ditentukan oleh Koyeb
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
